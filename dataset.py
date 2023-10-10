44e747105c1f4dc73772f7bb996f2f5da3c66a8b
amonleong <amonleong@ub.edu>
2023-10-10 11:44:09 +0200

    más cambios

--- dataset.py
+++ dataset.py
@@ -1,25 +1,35 @@
 import os
-import zipfile as zf
-import tarfile as tf
+import zipfile
+import shutil
 
 
 class Dataset:
-    def __init__(self, origin: str, destination: str):
+    def __init__(self, origin: str, destination: str) -> None:
         self.origin = origin
         self.destination = destination
 
-
-    def decompress(self):
+    def decompress(self) -> None:
         directories: list[str] = os.listdir(self.origin)
         for directory in directories:
             path: str = os.path.join(self.origin, directory)
-            with zf.ZipFile(path, 'r') as zip:
+            with zipfile.ZipFile(path, 'r') as zip:
                 zip.extractall(self.destination)
+
+    def copy(self):
+        sequence: int = 1
+        directories: list[str] = os.listdir(self.destination)
+        for directory in directories:
+            path: str = os.path.join(self.destination, directory)
+            files: list[str] = os.listdir(path)
             
+            for file in files:
+                extension: str = os.path.splitext(file)[1]
+                filename: str = f'frame_{sequence:04d}{extension}'
+                origin: str = os.path.join(self.destination, directory, file)
+                destination: str = os.path.join(self.destination, filename)
 
-
-ola = Dataset('D:/thetys_mariadelahoz_6_2023', 'D:/prueba')
-ola.decompress()
-        
+                shutil.copy(origin, destination)
+                sequence += 1
+            
         
     
