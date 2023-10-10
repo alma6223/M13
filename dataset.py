4cd5e03f88bf36397a42628a43ccd0cbb6e847fb
amonleong <amonleong@ub.edu>
2023-10-10 10:58:50 +0200

    funcion decompress acabada

--- dataset.py
+++ dataset.py
@@ -1,25 +1,27 @@
 import os
 import zipfile as zf
+import tarfile as tf
 
 
 class Dataset:
-    def __init__(self, origin, destination):
+    def __init__(self, origin: str, destination: str):
         self.origin = origin
         self.destination = destination
-        self.directories = os.listdir(self.origin)
 
 
     def decompress(self):
-        size = len(self.directories)
-        for index in range(size):
-            path = f'{self.origin}/{self.directories[index]}'
-            zips = os.listdir(path)
-            with zf.ZipFile(zips, 'r') as zip:
-                zip.extractall(self.origin)
+        directories: list[str] = os.listdir(self.origin)
+        for directory in directories:
+            path: str = os.path.join(self.origin, directory)
+            with zf.ZipFile(path, 'r') as zip:
+                zip.extractall(self.destination)
+
+
+    def 
             
 
 
-ola = Dataset('D:/thetys_mariadelahoz_6_2023', 'adad')
+ola = Dataset('D:/thetys_mariadelahoz_6_2023', 'D:/prueba')
 ola.decompress()
         
         
