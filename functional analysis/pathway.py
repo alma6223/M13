edb97c5735b4b545f8f0652f36342efc68fc70fe
Albert Martín Moreno <alma6223@office.proven.cat>
2023-10-24 11:32:03 +0200

    cambios

--- functional analysis/pathway.py
+++ functional analysis/pathway.py
@@ -1,46 +1,46 @@
 from bs4 import BeautifulSoup
 from typing import List, Dict
 import requests
+from requests.models import Response
 import json
 
 
-url: str = 'https://www.genome.jp/kegg/pathway.html'
+URL: str
+URL = 'https://www.genome.jp/kegg/pathway.html'
+
 
 try:
-    response: str = requests.get(url).text
+    response: Response
+    response = requests.get(URL)
 except requests.exceptions.RequestException as error:
     print(error)
 
 
-soup = BeautifulSoup(response, features='html.parser')
+soup: BeautifulSoup
+soup = BeautifulSoup(response.text, features='html.parser')
 
 
-groups: List[str] = []
+groups:  List[str]
+groups: List[str] = [group.text[0] for group in soup.find_all('b')[1:8]]
 
-for group in soup.find_all('b')[1:8]:
-    groups.append(group.text)
+subgroups: List[str]
+subgroups: List[str] = [subgroup.text.split()[0] for subgroup in soup.find_all('b')[8:]]
 
-
-subgroups: List[str] = []
-
-for subgroup in soup.find_all('b')[8:]:
-    subgroups.append(subgroup.text)
+entries: List[str]
+entries: List[str] = [entry.text for entry in soup.find_all(attrs={'class': 'list'})]
 
 
-codes: List[str] = []
-
-for code in soup.find_all(attrs={'class': 'list'}):
-    codes.append(code.text)
-
-
+data: Dict[str, Dict[str, List[str]]]
 data = {}
 
 for group in groups:
-    data[group[0]] = {}
+    data[group] = {}
     for index in range(len(subgroups)):
-        if subgroups[index][0] == group[0]:
-            data[group[0]][subgroups[index].split()[0]] = [code[:5] for code in codes[index].split('\n') if code != '']
+        if subgroups[index][0] == group:
+            data[group][subgroups[index]] = [entry[:5] for entry in entries[index].split('\n') if entry != '']
             
 
-with open('pathway.json', 'w') as f:
-    json.dump(data, f, indent=4)
+FILENAME: str
+FILENAME = 'pathway.json'
+with open(FILENAME, 'w') as file:
+    json.dump(data, file, indent=4)
