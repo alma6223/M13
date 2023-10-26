97d886429ea1cc73e85b7ca873a2b9e713d22830
Albert Martín Moreno <alma6223@office.proven.cat>
2023-10-26 12:00:02 +0200

    avanzando

--- functional_analysis/kegg.py
+++ functional_analysis/kegg.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from pandas import DataFrame
-import matplotlib.pyplot as plt
 import json
 from bs4 import BeautifulSoup
 import requests
@@ -10,16 +9,24 @@ from typing import List, Dict
 
 
 class KEGG:
-    def __init__(self,  filepath_or_buffer: str):
-        self.data_frame: DataFrame = pd.read_csv(filepath_or_buffer = filepath_or_buffer, delimiter = ',')
+    def __init__(self,  filepath_or_buffer: str, groups: List[str], columns: List[str]):
+        self.filepath_or_buffer = filepath_or_buffer
+        self.groups = groups
+        self.columns = columns
 
 
-    def pathway(self):
+    def data_frame(self) -> DataFrame:
+        data_frame: DataFrame = pd.read_csv(filepath_or_buffer = self.filepath_or_buffer, delimiter = ',')
+        data_frame: DataFrame = data_frame[data_frame[self.columns[0]].isin(self.groups)]
+        return data_frame
+
+
+    def pathway(self) -> Dict[str, Dict[str, List[str]]]:
         url: str = 'https://www.genome.jp/kegg/pathway.html'
-        response: Response = requests.get(url = url)
+        markup: Response = requests.get(url = url)
 
 
-        soup: BeautifulSoup = BeautifulSoup(markup = response.text, features = 'html.parser')        
+        soup: BeautifulSoup = BeautifulSoup(markup = markup.text, features = 'html.parser')        
         groups: List[str] = [group.text[0] for group in soup.find_all('b')[1:8]]
         subgroups: List[str] = [subgroup.text.split()[0] for subgroup in soup.find_all('b')[8:]]
         entries: List[str] = [entry.text for entry in soup.find_all(attrs={'class': 'list'})]
@@ -35,15 +42,21 @@ class KEGG:
 
         file: str = 'pathway.json'
         with open(file = file, mode = 'w') as fp:
-            json.dump(obj = obj, fp = fp)
+            json.dump(obj = obj, fp = fp, indent=4)
 
-
-    def entries(self, groups: List[str], columns: List[str]) -> DataFrame:
-        """
         
-        """
-        entries: DataFrame = pd.concat([self.data_frame[self.data_frame[columns[0]].isin(groups)]])
-        entries: DataFrame = entries.groupby(by = [columns[0], columns[1]]).size().reset_index(name = 'COUNT')
+        return obj
+
+
+    def entries(self, data_frame: DataFrame) -> DataFrame:
+        entries: DataFrame = data_frame.groupby(by = [self.columns[0], self.columns[1]]).size().reset_index(name = 'COUNT')
         path_or_buf: str = 'entries.csv'
         entries.to_csv(path_or_buf = path_or_buf)
         return entries
+    
+
+    
+    
+    
+pp = KEGG('data.csv', [], [])
+
